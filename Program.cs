69910cea1eaa214ad8d13d7b757862416137381e
iremkabil <iremkabil54@gmail.com>
2025-12-18 23:41:52 +0300

    Admin Dashboard açılıyor.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpClient();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
