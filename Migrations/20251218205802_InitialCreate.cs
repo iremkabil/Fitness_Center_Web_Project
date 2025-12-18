76e20ec1ebfd446582f3fd15b0037c5a850bdb94
iremkabil <iremkabil54@gmail.com>
2025-12-18 23:59:30 +0300

    Migration oluşturuldu

---
+++ Migrations/20251218205802_InitialCreate.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fitness_Center_Web_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kazanclar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ay = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    kazanc = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kazanclar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personeller",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Soyad = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Cinsiyet = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Telefon = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Biyografi = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    AktifMi = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personeller", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Soyad = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Telefon = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Sifre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Uzmanliklar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UzmanlikAdi = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    AktifMi = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Uzmanliklar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mesailer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonelId = table.Column<int>(type: "int", nullable: false),
+                    BaslangicZamani = table.Column<TimeSpan>(type: "time", nullable: false),
+                    BitisZamani = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mesailer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mesailer_Personeller_PersonelId",
+                        column: x => x.PersonelId,
+                        principalTable: "Personeller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Islemler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Ucret = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Sure = table.Column<TimeSpan>(type: "time", nullable: false),
+                    UzmanlikId = table.Column<int>(type: "int", nullable: true),
+                    Aciklama = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    AktifMi = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Islemler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Islemler_Uzmanliklar_UzmanlikId",
+                        column: x => x.UzmanlikId,
+                        principalTable: "Uzmanliklar",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonelUzmanliklar",
+                columns: table => new
+                {
+                    PersonellerId = table.Column<int>(type: "int", nullable: false),
+                    UzmanliklarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonelUzmanliklar", x => new { x.PersonellerId, x.UzmanliklarId });
+                    table.ForeignKey(
+                        name: "FK_PersonelUzmanliklar_Personeller_PersonellerId",
+                        column: x => x.PersonellerId,
+                        principalTable: "Personeller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonelUzmanliklar_Uzmanliklar_UzmanliklarId",
+                        column: x => x.UzmanliklarId,
+                        principalTable: "Uzmanliklar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MesaiGunleri",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MesaiId = table.Column<int>(type: "int", nullable: false),
+                    Gun = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MesaiGunleri", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MesaiGunleri_Mesailer_MesaiId",
+                        column: x => x.MesaiId,
+                        principalTable: "Mesailer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Randevular",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    PersonelId = table.Column<int>(type: "int", nullable: false),
+                    IslemId = table.Column<int>(type: "int", nullable: false),
+                    RandevuTarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RandevuSaati = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Durum = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Sure = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Ucret = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Not = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Randevular", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Randevular_Islemler_IslemId",
+                        column: x => x.IslemId,
+                        principalTable: "Islemler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Randevular_Personeller_PersonelId",
+                        column: x => x.PersonelId,
+                        principalTable: "Personeller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Randevular_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Islemler_UzmanlikId",
+                table: "Islemler",
+                column: "UzmanlikId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MesaiGunleri_MesaiId",
+                table: "MesaiGunleri",
+                column: "MesaiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mesailer_PersonelId",
+                table: "Mesailer",
+                column: "PersonelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonelUzmanliklar_UzmanliklarId",
+                table: "PersonelUzmanliklar",
+                column: "UzmanliklarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Randevular_IslemId",
+                table: "Randevular",
+                column: "IslemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Randevular_PersonelId",
+                table: "Randevular",
+                column: "PersonelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Randevular_UserId",
+                table: "Randevular",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kazanclar");
+
+            migrationBuilder.DropTable(
+                name: "MesaiGunleri");
+
+            migrationBuilder.DropTable(
+                name: "PersonelUzmanliklar");
+
+            migrationBuilder.DropTable(
+                name: "Randevular");
+
+            migrationBuilder.DropTable(
+                name: "Mesailer");
+
+            migrationBuilder.DropTable(
+                name: "Islemler");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Personeller");
+
+            migrationBuilder.DropTable(
+                name: "Uzmanliklar");
+        }
+    }
+}
