3c97a0ee2822715d6d4d62c4788f47f42f7ab6e8
fatih çiloğlu <121500177+faithofalbus@users.noreply.github.com>
2025-12-19 08:43:22 +0300

    AI entegrasyonu için gerekli veri modeli (AiOneriModel) oluşturuldu

---
+++ Models/AiOneriModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Fitness_Center_Web_Project.Models
+{
+    public class AiOneriModel
+    {
+        // Kullanıcıdan alacağımız bilgiler
+        public int Yas { get; set; }
+        public int Boy { get; set; }   // cm
+        public int Kilo { get; set; }  // kg
+        public string Cinsiyet { get; set; } = "Belirtilmemiş";
+        public string Hedef { get; set; } = "Kilo Vermek";
+
+        // Yapay zekadan gelen cevabı buraya yazacağız
+        public string? YapayZekaCevabi { get; set; }
+    }
+}
